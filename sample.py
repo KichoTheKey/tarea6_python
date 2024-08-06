a1292783d39a34eaedcca360a29363b18298cbfa
Renato <renato3gonzalez@gmail.com>
2024-08-05 23:00:41 -0400

    tercera versión

--- sample.py
+++ sample.py
@@ -1,20 +1,37 @@
-posts = []
-
 while True:
-    print("Introduzca una puntuación utilizando un número del 1 al 5. Introduzca '6' para salir")
-    point = input()
-    if point.isdecimal():
-        point = int(point)
-        if point == 6:
-            print("Terminación.")
+    print('Seleccione el proceso que desea aplicar')
+    print('1: Ingresa calificación y comentario')
+    print('2: Comprueba los resultados obtenidos hasta ahora')
+    print('3: Fin')
+    num = input()
+    
+    if num.isdecimal():
+        num = int(num)
+        if num == 1:
+            print('Introduzca una puntuación en una escala del 1 al 5')
+            puntos = input()
+            if puntos.isdecimal():
+                puntos = int(puntos)
+                if puntos <= 0 or puntos > 5:
+                    print('Debe estar en una esacala de 1 a 5')
+                else:
+                    print('Ingrese el comentario')
+                    comentario = input()
+                    post = f'Puntos: {puntos}. Comentario: {comentario}'
+                    file_pc = open("data.txt", 'a')
+                    file_pc.write(f'{post} \n')
+                    file_pc.close()
+            else:
+                print('Debe ser un número del 1 al 5')
+        elif num == 2:
+            print('Resultados hasta la fecha')
+            read_file = open("data.txt", "r")
+            print(read_file.read())
+            read_file.close()
+        elif num == 3:
+            print('Fin')
             break
-        elif 1 <= point <= 5:
-            print("Introduzca sus comentarios.")
-            comment = input()
-            post = {'point': point, 'comment': comment}
-            posts.extfin(post.items())
-            print(posts)
         else:
-            print("Introduzca un número del 1 al 6")
+            print('Introduzca un número del 1 al 3')
     else:
-        print("Introduzca un número")
+        print('Introduzca un número del 1 al 3')
