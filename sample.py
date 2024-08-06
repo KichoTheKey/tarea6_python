9e8b3063d90ca26386a0492d6081a087658be54d
Renato <renato3gonzalez@gmail.com>
2024-08-05 23:18:54 -0400

    refactorización de python

--- sample.py
+++ sample.py
@@ -1,33 +1,41 @@
+def ingresar_calificacion():
+    print('Introduzca una puntuación en una escala del 1 al 5')
+    puntos = input()
+    if puntos.isdecimal():
+        puntos = int(puntos)
+        if puntos <= 0 or puntos > 5:
+            print('Debe estar en una escala de 1 a 5')
+        else:
+            print('Ingrese el comentario')
+            comentario = input()
+            post = f'Puntos: {puntos}. Comentario: {comentario}'
+            with open("data.txt", 'a') as file_pc:
+                file_pc.write(f'{post}\n')
+    else:
+        print('Debe ser un número del 1 al 5')
+
+def comprobar_resultados():
+    print('Resultados hasta la fecha')
+    with open("data.txt", "r") as read_file:
+        contenido = read_file.read()
+        if contenido:
+            print(contenido)
+        else:
+            print("Todavía no hay resultados disponibles")
+
 while True:
     print('Seleccione el proceso que desea aplicar')
     print('1: Ingresa calificación y comentario')
     print('2: Comprueba los resultados obtenidos hasta ahora')
     print('3: Fin')
     num = input()
-    
+        
     if num.isdecimal():
         num = int(num)
         if num == 1:
-            print('Introduzca una puntuación en una escala del 1 al 5')
-            puntos = input()
-            if puntos.isdecimal():
-                puntos = int(puntos)
-                if puntos <= 0 or puntos > 5:
-                    print('Debe estar en una esacala de 1 a 5')
-                else:
-                    print('Ingrese el comentario')
-                    comentario = input()
-                    post = f'Puntos: {puntos}. Comentario: {comentario}'
-                    file_pc = open("data.txt", 'a')
-                    file_pc.write(f'{post} \n')
-                    file_pc.close()
-            else:
-                print('Debe ser un número del 1 al 5')
+            ingresar_calificacion()
         elif num == 2:
-            print('Resultados hasta la fecha')
-            read_file = open("data.txt", "r")
-            print(read_file.read())
-            read_file.close()
+            comprobar_resultados()
         elif num == 3:
             print('Fin')
             break
